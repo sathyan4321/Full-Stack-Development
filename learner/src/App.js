358503790f6f879e2a4e3f440fbe2360e9b6b0d8
sathyan4321 <sathyanr.4321@gmail.com>
2024-05-06 16:14:47 +0530

    Second commit

---
+++ learner/src/App.js
@@ -0,0 +1,22 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>header</h1>
+      </header>
+      <main className="main">
+        <div className="sider">
+          <h1>sider</h1>
+        </div>
+
+        <div className="content">
+          <h1>content</h1>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
